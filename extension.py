beba89f5def34ded93110ecdac3866eff231320d
Caitlin B <caitlin.blackall@gmail.com>
2023-07-14 21:49:27 +0200

    removed api id and password

--- extension.py
+++ extension.py
@@ -4,8 +4,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 # Ask the user to enter a cuisine type
 cuisine_type = input("Enter a cuisine type to search for: ")
 
-# Use the Spotify API to search for playlists related to the cuisine type
-client_credentials_manager = SpotifyClientCredentials(client_id='01b9516fd1224b83b1c4362ba4b15b2e', client_secret='97375c132b5f4bcb98f379b11ccc5878')
+# Use the Spotify API to search for playlists related to the cuisine type - need to add client_id and client_secret
+client_credentials_manager = SpotifyClientCredentials(client_id='''01b9516fd1224b83b1c4362ba4b15b2e''', client_secret='''97375c132b5f4bcb98f379b11ccc5878''')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 results = sp.search(q=cuisine_type, type='playlist')
 
